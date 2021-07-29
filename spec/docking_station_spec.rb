ff042d7214462d2b85ade98763ea7760a3e96ca3
danroche10 <danielroche10@gmail.com>
2021-07-29 17:37:12 +0100

    step 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,32 +2,38 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-   before(:each) do
-     @docking_station = DockingStation.new
-   end
-  
-  # it "respond to release bike method" do
-  #   expect(@docking_station).to respond_to('release_bike')
-  # end
-  
-  # one line version of above
+
   # test 1
-  subject { @docking_station }
-  it { respond_to('release_bike') }
+  it "respond to release bike method" do
+    expect(subject).to respond_to('release_bike')
+  end
 
   # test 2 
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end
+
   # test 3
   it { is_expected.to respond_to(:dock).with(1).argument }
+
   # test 4
   it { is_expected.to respond_to(:bike) }
+
   # test 5
   it "returns docked bikes" do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+
+    # test 6
+    it "raise error of there are no bikes" do
+      # Let's not dock a bike first:
+      # remember subject == DockingStation.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 end
