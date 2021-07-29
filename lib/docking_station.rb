ad5b9b821265214d648c1170c8a9010424da11f9
danroche10 <danielroche10@gmail.com>
2021-07-29 15:05:59 +0100

    step 10 complete with tests passing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,8 @@
-require './bike.rb'
+require_relative 'bike'
 
 class DockingStation
   def release_bike
     # returns an instance of the bike class 
     Bike.new
-
   end
 end
-
-# Class is now defined so no longer recieveing error
-
-# release_bike method undefined so now no method error appearing
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,19 +9,16 @@ describe DockingStation do
   # it "respond to release bike method" do
   #   expect(@docking_station).to respond_to('release_bike')
   # end
-  # one line version of above
   
+  # one line version of above
   # test 1
   subject { @docking_station }
   it { respond_to('release_bike') }
+
   # test 2 
-
-  # it "responds to working? in bike class" do
-  #   expect(something).to eq(true)
-  # end
- 
-  # it { respond_to('working?')}
-  # same non-failing issue as before ? 
-
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
 
