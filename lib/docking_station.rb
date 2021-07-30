875c7b99dfc2d3595304b1aaa7a8917f0e729bfa
danroche10 <danielroche10@gmail.com>
2021-07-30 16:48:26 +0100

    stage 15 complete

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,18 +6,25 @@ class DockingStation
     @bikes = []
   end
 
+  
   def release_bike
-    fail 'No bikes available' unless bikes.length > 0
+    fail 'No bikes available' unless empty?
     bikes
   end
 
   def dock(bike)
-    fail 'Station is at full capacity' if bikes.length >= 20
+    fail 'Station is at full capacity' if full?
     bikes << bike
   end
-end
 
-# docking_station = DockingStation.new
-# docking_station.dock(Bike.new)
-# docking_station.dock(Bike.new)
+  private
+
+  def empty?
+    bikes.length > 0 ? true : false
+  end
+
+  def full?
+    bikes.length >= 20 ? true : false
+  end
+end
 
